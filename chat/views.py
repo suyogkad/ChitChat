fe83f0e1013dfa790536b85ae64bf2bd8bd2996e
suyogkad <7suyog@gmail.com>
2024-02-02 19:40:51 +0545

    Function to Call Registered Users Updated

--- chat/views.py
+++ chat/views.py
@@ -1,33 +1,15 @@
-from django.shortcuts import render
-from .models import Message
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from .forms import SignUpForm
-from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import Message
-from .forms import MessageForm
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required  # Import this
-from .models import Message
-from .forms import MessageForm
+from .forms import SignUpForm, MessageForm
 
-def message_list(request):
-    if request.method == 'POST':
-        form = MessageForm(request.POST)
-        if form.is_valid():
-            msg = form.save(commit=False)
-            msg.sender = request.user
-            msg.save()
-            return redirect('message_list')
-    else:
-        form = MessageForm()
-
-    messages = Message.objects.all()
-    return render(request, 'chat/message_list.html', {'messages': messages, 'form': form})
 
 def home(request):
     return HttpResponse('<h1>Welcome to ChitChat!</h1><p><a href="/chat/messages/">Go to Chat Messages</a></p>')
+
 
 def signup(request):
     if request.method == 'POST':
@@ -40,7 +22,8 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'chat/signup.html', {'form': form})
 
-@login_required  # Decorator to ensure only logged-in users can access this view
+
+@login_required
 def message_list(request):
     if request.method == 'POST':
         form = MessageForm(request.POST)
@@ -52,10 +35,9 @@ def message_list(request):
     else:
         form = MessageForm()
 
-    # Filtering messages for the current user, either sent or received
+    users = User.objects.exclude(id=request.user.id)  # Exclude the current user
     messages = Message.objects.filter(sender=request.user) | Message.objects.filter(receiver=request.user)
 
-    return render(request, 'chat/message_list.html', {'messages': messages.order_by('-timestamp'), 'form': form})
-
-
-
+    return render(request, 'chat/message_list.html', {
+        'users': users, 'messages': messages.order_by('-timestamp'), 'form': form
+    })
